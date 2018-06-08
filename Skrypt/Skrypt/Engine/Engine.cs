eaeed7e8ead0e51f35ecf610b97e89d255d6b86b
Blieper <yoranmandema@eml.cc>
2018-06-08 20:26:18 +0200

    SkryptEngine class

---
+++ Skrypt/Skrypt/Engine/Engine.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Skrypt.Tokenization;
+using System.Text.RegularExpressions;
+using System.Diagnostics; // Using this so we can check how fast everything is happening
+
+namespace Skrypt.Engine {
+    class SkryptEngine {
+        Tokenizer tokenizer = new Tokenizer();
+        List<Token> Tokens;
+
+        public SkryptEngine() {
+            // Tokens that are found using a token rule with type defined as 'null' won't get added to the token list.
+            // This means you can ignore certain characters, like whitespace in this case, that way.
+            tokenizer.AddRule(
+                new Regex(@"\s"),
+                null
+            );
+
+            tokenizer.AddRule(
+                new Regex(@"\d+(\.\d+)?"),
+                "NumericLiteral"
+            );
+
+            tokenizer.AddRule(
+                new Regex(@"[_a-zA-Z]+[_a-zA-Z0-9]*"),
+                "Identifier"
+            );
+
+            tokenizer.AddRule(
+                new Regex("true|false"),
+                "BooleanLiteral"
+            );
+
+            tokenizer.AddRule(
+                new Regex(@"(&&)|(\|\|)|(\|\|\|)|(==)|(!=)|(>=)|(<=)|(<<)|(>>)|(>>>)|(\+\+)|(--)|[~=;:<>+\-*/%^&|!\[\]\(\)\.\,{}]"),
+                "Punctuator"
+            );
+
+            tokenizer.AddRule(
+                new Regex(@""".*?(?<!\\)"""),
+                "StringLiteral"
+            );
+
+            // Multi line comment
+            tokenizer.AddRule(
+                new Regex(@"\/\*(.|\n)*\*\/"),
+                null
+            );
+
+            // Single line comment
+            tokenizer.AddRule(
+                new Regex(@"\/\/.*\n"),
+                null
+            );
+        }
+
+        public void Run (string code) {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            Tokens = tokenizer.Tokenize(code);
+
+            if (Tokens == null) { return; }
+
+            TokenProcessor.ProcessTokens(Tokens);
+            sw.Stop();
+
+            // Debug token list print.
+            if (Tokens != null) {
+                foreach (Token token in Tokens) {
+                    Console.WriteLine(token);
+                }
+            }
+
+            Console.WriteLine("Tokenization time: {0} ms", sw.Elapsed.Milliseconds);
+        }
+    }
+}
--- Skrypt/Skrypt/Program.cs
+++ Skrypt/Skrypt/Program.cs
@@ -4,78 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Skrypt.Tokenization;
+using Skrypt.Engine;
 using System.Text.RegularExpressions;
 using System.IO;
-using System.Diagnostics; // Using this so we can check how fast everything is happening
 
 namespace Skrypt {
     class Program {
         static void Main(string[] args) {
-            Stopwatch sw = new Stopwatch();
-            Tokenizer T = new Tokenizer();
-
-            // Tokens that are found using a token rule with type defined as 'null' won't get added to the token list.
-            // This means you can ignore certain characters, like whitespace in this case, that way.
-            T.AddRule(
-                new Regex(@"\s"),
-                null
-            );
-
-            T.AddRule(
-                new Regex(@"\d+(\.\d+)?"),
-                "NumericLiteral"
-            );
-
-            T.AddRule(
-                new Regex(@"[_a-zA-Z]+[_a-zA-Z0-9]*"),
-                "Identifier"
-            );
-
-            T.AddRule(
-                new Regex("true|false"),
-                "BooleanLiteral"
-            );
-
-            T.AddRule(
-                new Regex(@"(&&)|(\|\|)|(\|\|\|)|(==)|(!=)|(>=)|(<=)|(<<)|(>>)|(>>>)|(\+\+)|(--)|[~=;:<>+\-*/%^&|!\[\]\(\)\.\,{}]"),
-                "Punctuator"
-            );
-
-            T.AddRule(
-                new Regex(@""".*?(?<!\\)"""),
-                "StringLiteral"
-            );
-
-            // Multi line comment
-            T.AddRule(
-                new Regex(@"\/\*(.|\n)*\*\/"),
-                null
-            );
-
-            // Single line comment
-            T.AddRule(
-                new Regex(@"\/\/.*\n"),
-                null
-            );
-
             var path = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), @"..\..\SkryptFiles\testcode.txt");
             var code = File.ReadAllText(path);
 
-            sw.Start();
-            // Tokenizing the file.
-            var Tokens = T.Tokenize(code);
-            if (Tokens != null)
-                TokenProcessor.ProcessTokens(Tokens);
-            sw.Stop();
+            SkryptEngine engine = new SkryptEngine();
 
-            // Debug token list print.
-            if (Tokens != null) {
-                foreach (Token token in Tokens) {
-                    Console.WriteLine(token);
-                }
-            }
-
-            Console.WriteLine("Tokenization time: {0} ms", sw.Elapsed.Milliseconds);
+            engine.Run(code);
 
             Console.ReadKey();
         }
