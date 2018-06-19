c96d1206232c6299a8eda259a2e6f1ad3678cc9b
Blieper <yoranmandema@eml.cc>
2018-06-19 17:34:59 +0200

    general parser comments + simplification

--- Skrypt/Skrypt/Parsing/GeneralParser.cs
+++ Skrypt/Skrypt/Parsing/GeneralParser.cs
@@ -6,22 +6,22 @@ using System.Threading.Tasks;
 using Skrypt.Tokenization;
 
 namespace Skrypt.Parsing {
+    /// <summary>
+    /// The general parser class.
+    /// Contains all methods to parse higher-level code, e.g code that contains statements AND expressions
+    /// </summary>
     static class GeneralParser {
         public static Node Parse(List<Token> Tokens) {
+            // Create main node
             Node Node = new Node { Body = "Block", TokenType = "Block" };
 
+            // Loop through all tokens
             for (int i = 0; i < Tokens.Count - 1; i++) {
                 var ExpressionNode = ExpressionParser.Parse(Tokens, ref i);
-
-                if (ExpressionNode != null) {
-                    Node.Add(ExpressionNode);
-                }
+                Node.Add(ExpressionNode);
 
                 var StatementNode = StatementParser.Parse(Tokens, ref i);
-
-                if (StatementNode != null) { 
-                    Node.Add(StatementNode);
-                }
+                Node.Add(StatementNode);
             }
 
             return Node;
