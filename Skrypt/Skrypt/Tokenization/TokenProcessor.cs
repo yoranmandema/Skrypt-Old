45be49bb50c150b5bb49e29b6606faad8e468633
Blieper <yoranmandema@eml.cc>
2018-06-19 18:06:23 +0200

    token processor comments

--- Skrypt/Skrypt/Tokenization/TokenProcessor.cs
+++ Skrypt/Skrypt/Tokenization/TokenProcessor.cs
@@ -6,12 +6,22 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
 namespace Skrypt.Tokenization {
+    /// <summary>
+    /// The token processor class.
+    /// Contains all methods to process existing tokens
+    /// </summary>
     static class TokenProcessor {
+        /// <summary>
+        /// Unescape string and remove outer " characters
+        /// </summary>
         static void ProcessStringToken (Token token) {
             token.Value = token.Value.Substring(1, token.Value.Length - 2);    
             token.Value = Regex.Unescape(token.Value);
         }
 
+        /// <summary>
+        /// Process all tokens in a list
+        /// </summary>      
         static public void ProcessTokens (List<Token> Tokens) {
             foreach (Token token in Tokens) {
                 switch (token.Type) {
