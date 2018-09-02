33599972572408d4295ff8ea140441344c6e2ae9
Blieper <yoranmandema@eml.cc>
2018-09-02 14:44:53 +0200

    Initial Commit

---
+++ Skrypt/Skrypt.LanguageServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace SampleServer {
+    class Program {
+        static void Main(string[] args) {
+            MainAsync(args).Wait();
+        }
+
+        static async Task MainAsync(string[] args) {
+
+        }
+    }
+}
