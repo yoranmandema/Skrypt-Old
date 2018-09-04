f0a855712b913324fcbb8ba858ba4db4dcef432a
Blieper <yoranmandema@eml.cc>
2018-09-04 19:16:49 +0200

    Revert "Revert "Setup basic typescript LSP""
    This reverts commit d932453acd703acda370b17ecad2437a3dd5a5a3.

--- skrypt-vscode/extension/client/src/test/index.ts
+++ skrypt-vscode/extension/client/src/test/index.ts
@@ -3,12 +3,13 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const testRunner = require("vscode/lib/testrunner");
+
+import * as testRunner from 'vscode/lib/testrunner';
+
 testRunner.configure({
     ui: 'bdd',
     useColors: true,
     timeout: 100000
 });
+
 module.exports = testRunner;
-//# sourceMappingURL=index.js.map
