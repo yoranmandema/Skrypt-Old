ca9f521f31dda5c882259d880f8f1fed8772a12c
Blieper <yoranmandema@eml.cc>
2018-10-07 19:38:11 +0200

    Cleanup

--- Skrypt/Skrypt/Parsing/StatementParser.cs
+++ Skrypt/Skrypt/Parsing/StatementParser.cs
@@ -13,19 +13,17 @@ namespace Skrypt.Parsing
     {
         private readonly SkryptEngine _engine;
 
-        public StatementParser(SkryptEngine e)
-        {
+        public StatementParser(SkryptEngine e) {
             _engine = e;
         }
 
         /// <summary>
         ///     Parses a statement ([if,while] (expression) {block}) into a node
         /// </summary>
-        public ParseResult ParseStatement(List<Token> tokens)
-        {
+        public ParseResult ParseStatement(List<Token> tokens) {
             var index = 0;
-            SkipInfo skip;
-            ParseResult result;
+            var skip = default(SkipInfo);
+            var result = default(ParseResult);
 
             // Create main statement node
             var node = new Node {Body = tokens[index].Value, Type = TokenTypes.Statement};
@@ -37,13 +35,14 @@ namespace Skrypt.Parsing
             var conditionNode = result.Node;
             index += result.Delta;
 
-            var conditionParentNode = new Node
-            {
+            var conditionParentNode = new Node {
                 Body = "Condition",
                 Type = TokenTypes.Condition
             };
 
-            if (conditionNode == null) _engine.ThrowError("Condition can't be empty!", tokens[index - 1]);
+            if (conditionNode == null) {
+                _engine.ThrowError("Condition can't be empty!", tokens[index - 1]);
+            }
 
             conditionParentNode.Add(conditionNode);
 
@@ -63,9 +62,9 @@ namespace Skrypt.Parsing
 
         public ParseResult ParseForStatement(List<Token> tokens) {
             var index = 0;
-            SkipInfo skip;
-            ParseResult result;
-            Node node = new Node { Body = "for", Type = TokenTypes.Statement };
+            var skip = default(SkipInfo);
+            var result = default(ParseResult);
+            var node = new Node { Body = "for", Type = TokenTypes.Statement };
 
             skip = _engine.ExpectValue("(", tokens);
             index += skip.Delta;
@@ -73,14 +72,14 @@ namespace Skrypt.Parsing
             index += skip.Delta;
 
             var conditionTokens = tokens.GetRange(skip.Start + 1, skip.Delta - 1);
-            ParseResult test;
+            var conditionResult = default(ParseResult);
             int i;
 
             for (i = 0; i < conditionTokens.Count; i++) {
-                test = _engine.ExpressionParser.Parse(conditionTokens.GetRange(i, conditionTokens.Count - i));
-                i += test.Delta - 1;
+                conditionResult = _engine.ExpressionParser.Parse(conditionTokens.GetRange(i, conditionTokens.Count - i));
+                i += conditionResult.Delta - 1;
 
-                node.Add(test.Node);
+                node.Add(conditionResult.Node);
             }
 
             if (node.Nodes.Count != 3) {
@@ -116,16 +115,14 @@ namespace Skrypt.Parsing
         /// <summary>
         ///     Parses an else statement (else {block})
         /// </summary>
-        public ParseResult ParseElseStatement(List<Token> tokens)
-        {
+        public ParseResult ParseElseStatement(List<Token> tokens) {
             var index = 0;
-            SkipInfo skip;
+            var skip = default(SkipInfo);
 
             skip = _engine.ExpectValue("{", tokens);
             index += skip.Delta;
 
-            var result =
-                _engine.GeneralParser.ParseSurrounded("{", "}", index, tokens, _engine.GeneralParser.Parse);
+            var result = _engine.GeneralParser.ParseSurrounded("{", "}", index, tokens, _engine.GeneralParser.Parse);
             var node = result.Node;
             node.Body = "else";
             index += result.Delta + 1;
@@ -145,24 +142,24 @@ namespace Skrypt.Parsing
             index += result.Delta;
 
             // Only parse statements elseif/else if there's any tokens after if statement
-            if (index < tokens.Count - 1)
-                while (tokens[index].Equals("elseif", TokenTypes.Keyword))
-                {
+            if (index < tokens.Count - 1) {
+                while (tokens[index].Equals("elseif", TokenTypes.Keyword)) {
                     var elseIfResult = ParseStatement(tokens.GetRange(index, tokens.Count - index));
                     result.Node.Add(elseIfResult.Node);
                     index += elseIfResult.Delta;
 
                     if (index == tokens.Count) break;
                 }
+            }
 
-            if (index < tokens.Count - 1)
-                if (tokens[index].Equals("else", TokenTypes.Keyword))
-                {
+            if (index < tokens.Count - 1) {
+                if (tokens[index].Equals("else", TokenTypes.Keyword)) {
                     var elseResult = ParseElseStatement(tokens.GetRange(index, tokens.Count - index));
                     result.Node.Add(elseResult.Node);
                     index += elseResult.Delta;
                 }
-
+            }
+            
             return new ParseResult {Node = result.Node, Delta = index};
         }
 
